cc44b5366e169d123591315234a10967414a1fa1
Anna <annajaninaschmidt@gmail.com>
2022-12-17 19:51:21 +1100

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
+var questions = [
+    {
+        question: 'What does JS stands for?',
+        answers: [
+            { text: 'JavaScrip', yay: true},
+            { text: 'No clue', nope: false},
+            { text: 'Jil Sander', nope: false},
+            { text: 'New show on Netflix', nope: false}
+        ]
+    },
+    {
+        question: 'What is Python?',
+        answers: [
+            { text: 'Dangerous snake', nope: false},
+            { text: 'Programming language', yay: true},
+            { text: 'Cute snake', nope: false},
+            { text: 'Fancy shoe brand', nope: false}
+        ]
+    }, 
+    {
+        question: 'HTML stands for?',
+        answer: [
+            { text: 'Hyper Text Makeup Lipstick', nope: false},
+            { text: 'Hyper Text Mouth Language', nope: false},
+            { text: 'Hyper Text Markup Language', yay: true},
+            { text: 'Hyper Text Moschino Label', nope: false}
+        ]
+    },
+    {
+        question: 'CSS stands for?',
+        answer: [
+            { text: 'Caroline Snake Shoes', nope: false},
+            { text: 'Christian Slater Son', nope: false},
+            { text: 'No idea', nope: false},
+            { text: 'Cascading Style Sheets', yay: true}
+        ]
+    }
+]
+
+
 var startButtonQuiz = document.getElementById('start-btn');
 var questionElement = document.getElementById('question');
 var answerElement = document.getElementById('answer')
@@ -25,14 +65,3 @@ function getTheAnswer() {
 
 }
 
-var questions = [
-    {
-        question: 'What does JS stands for?',
-        answers: [
-            { text: 'JavaScrip', yay: true},
-            { text: 'No clue', nope: false},
-            { text: 'Jil Sander', nope: false},
-            { text: 'New show on Netflix', nope: false}
-        ]
-    }
-]
